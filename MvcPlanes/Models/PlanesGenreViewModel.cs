f7bf0e750a6982be95267098446eec9f21eed00a
DarrenUuskula <115153527+DarrenUuskula@users.noreply.github.com>
2023-01-25 00:56:40 +0200

    Add search by genre to the Index view

--- MvcPlanes/Models/PlanesGenreViewModel.cs
+++ MvcPlanes/Models/PlanesGenreViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace MvcPlanes.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcPlanes.Models
 {
-    public class MovieGenreViewModel
+    public class PlanesGenreViewModel
     {
+        public List<Planes>? Planes { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? PlanesGenre { get; set; }
+        public string? SearchString { get; set; }
     }
 }
